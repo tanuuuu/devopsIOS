02621849de8a2de01fc18145149e1f11b86a297b
devopsIOS <tanuu096@gmail.com>
2024-04-02 14:18:18 +0530

    first commit

---
+++ devIosNew/devIosNew/devIosNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  devIosNewApp.swift
+//  devIosNew
+//
+//  Created by F on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct devIosNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
